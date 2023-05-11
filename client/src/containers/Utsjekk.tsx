7d8509e3b5abb2d617b70c0872b7e9f970e2cee8
Håkon <hakonhagen2@gmail.com>
2023-05-11 14:55:07 +0200

    Flyttet litt på innhold

--- client/src/containers/Utsjekk.tsx
+++ client/src/containers/Utsjekk.tsx
@@ -96,18 +96,6 @@ const Utsjekk = () => {
     return <>Fant ingen bestilling...</>
   }
 
-  if (henterRolle) {
-    return <Loader />
-  }
-
-  if (rolleResponse === undefined) {
-    return <Loader />
-  }
-
-  if (rolleResponse.kanBestilleDeler === false) {
-    return <>Du kan ikke bestille deler akkurat nå.</>
-  }
-
   return (
     <>
       <Header>
@@ -119,94 +107,109 @@ const Utsjekk = () => {
       </Header>
       <main>
         <Content>
-          {visFlereDeler && (
-            <Avstand marginBottom={2}>
-              <Button variant="tertiary" onClick={() => setVisFlereDeler(false)}>
-                Tilbake til bestillingen
-              </Button>
-            </Avstand>
-          )}
-          <Panel>
-            <Heading level="3" size="small" spacing>
-              Bestill deler til {bestilling.hjelpemiddel.navn}
-            </Heading>
-            <BodyShort>
-              <strong>Art.nr:</strong> {bestilling.hjelpemiddel.hmsnr} | <strong>Serienr:</strong>{' '}
-              {bestilling.handlekurv.serieNr}
-            </BodyShort>
-          </Panel>
-          <Avstand marginBottom={4} />
-          {visFlereDeler ? (
-            <>
-              <LeggTilDel
-                hjelpemiddel={{
-                  ...bestilling.hjelpemiddel,
-                  // Filtrer bort allerede lagt til deler
-                  deler: bestilling.hjelpemiddel.deler?.filter(
-                    (del) => !bestilling.handlekurv.deler.find((handlekurvDel) => handlekurvDel.hmsnr === del.hmsnr)
-                  ),
-                }}
-                onLeggTil={(del) => leggTilDel(del)}
-              />
-            </>
+          {henterRolle ? (
+            <div style={{ textAlign: 'center' }}>
+              <Loader />
+            </div>
           ) : (
             <>
-              <Avstand marginBottom={8}>
-                <Heading level="2" size="large" spacing>
-                  Deler lagt til i bestillingen
-                </Heading>
-                {bestilling.handlekurv.deler.length === 0 && <div>Du har ikke lagt til noen deler</div>}
-                {bestilling.handlekurv.deler.map((del) => (
-                  <Panel key={del.hmsnr} border>
-                    <Heading level="3" size="medium">
-                      {del.navn}
-                    </Heading>
-                    <BodyShort spacing>{del.beskrivelse}</BodyShort>
-                    <div
-                      style={{
-                        padding: '1rem',
-                        background: '#f1f1f1',
-                        display: 'flex',
-                        alignItems: 'flex-end',
-                        justifyContent: 'space-between',
-                      }}
-                    >
-                      <Button icon={<TrashIcon />} variant="tertiary" onClick={() => handleSlettDel(del)}>
-                        Slett del
+              {rolleResponse && rolleResponse.kanBestilleDeler === false ? (
+                <div>Du kan ikke bestille deler akkurat nå</div>
+              ) : (
+                <>
+                  {visFlereDeler && (
+                    <Avstand marginBottom={2}>
+                      <Button variant="tertiary" onClick={() => setVisFlereDeler(false)}>
+                        Tilbake til bestillingen
                       </Button>
-                      <Select
-                        label="Antall"
-                        value={del.antall}
-                        onChange={(e) => setAntall(del, Number(e.target.value))}
-                      >
-                        {Array.from(Array(5), (_, x: number) => (
-                          <option key={x + 1} value={x + 1}>
-                            {x + 1}
-                          </option>
-                        ))}
-                      </Select>
-                    </div>
+                    </Avstand>
+                  )}
+                  <Panel>
+                    <Heading level="3" size="small" spacing>
+                      Bestill deler til {bestilling.hjelpemiddel.navn}
+                    </Heading>
+                    <BodyShort>
+                      <strong>Art.nr:</strong> {bestilling.hjelpemiddel.hmsnr} | <strong>Serienr:</strong>{' '}
+                      {bestilling.handlekurv.serieNr}
+                    </BodyShort>
                   </Panel>
-                ))}
-                <Avstand marginBottom={4} />
-                <Button variant="secondary" onClick={() => setVisFlereDeler(true)}>
-                  Legg til flere deler
-                </Button>
-              </Avstand>
+                  <Avstand marginBottom={4} />
+                  {visFlereDeler ? (
+                    <>
+                      <LeggTilDel
+                        hjelpemiddel={{
+                          ...bestilling.hjelpemiddel,
+                          // Filtrer bort allerede lagt til deler
+                          deler: bestilling.hjelpemiddel.deler?.filter(
+                            (del) =>
+                              !bestilling.handlekurv.deler.find((handlekurvDel) => handlekurvDel.hmsnr === del.hmsnr)
+                          ),
+                        }}
+                        onLeggTil={(del) => leggTilDel(del)}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <Avstand marginBottom={8}>
+                        <Heading level="2" size="large" spacing>
+                          Deler lagt til i bestillingen
+                        </Heading>
+                        {bestilling.handlekurv.deler.length === 0 && <div>Du har ikke lagt til noen deler</div>}
+                        {bestilling.handlekurv.deler.map((del) => (
+                          <Panel key={del.hmsnr} border>
+                            <Heading level="3" size="medium">
+                              {del.navn}
+                            </Heading>
+                            <BodyShort spacing>{del.beskrivelse}</BodyShort>
+                            <div
+                              style={{
+                                padding: '1rem',
+                                background: '#f1f1f1',
+                                display: 'flex',
+                                alignItems: 'flex-end',
+                                justifyContent: 'space-between',
+                              }}
+                            >
+                              <Button icon={<TrashIcon />} variant="tertiary" onClick={() => handleSlettDel(del)}>
+                                Slett del
+                              </Button>
+                              <Select
+                                label="Antall"
+                                value={del.antall}
+                                onChange={(e) => setAntall(del, Number(e.target.value))}
+                              >
+                                {Array.from(Array(5), (_, x: number) => (
+                                  <option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                ))}
+                              </Select>
+                            </div>
+                          </Panel>
+                        ))}
+                        <Avstand marginBottom={4} />
+                        <Button variant="secondary" onClick={() => setVisFlereDeler(true)}>
+                          Legg til flere deler
+                        </Button>
+                      </Avstand>
 
-              <Avstand marginBottom={8}>
-                <Heading spacing level="3" size="medium">
-                  Levering
-                </Heading>
-                <div>TODO: implementer</div>
-              </Avstand>
+                      <Avstand marginBottom={8}>
+                        <Heading spacing level="3" size="medium">
+                          Levering
+                        </Heading>
+                        <div>TODO: implementer</div>
+                      </Avstand>
 
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 12 }}>
-                <Button>Send inn bestilling</Button>
-                <Button icon={<TrashIcon />} variant="tertiary" onClick={handleSlettBestilling}>
-                  Slett bestilling
-                </Button>
-              </div>
+                      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 12 }}>
+                        <Button>Send inn bestilling</Button>
+                        <Button icon={<TrashIcon />} variant="tertiary" onClick={handleSlettBestilling}>
+                          Slett bestilling
+                        </Button>
+                      </div>
+                    </>
+                  )}
+                </>
+              )}
             </>
           )}
         </Content>
