0b55398fb3503572230bc15d8348e66654e8d066
Håkon <hakonhagen2@gmail.com>
2023-06-02 13:03:10 +0200

    handleResponse

--- client/src/services/rest.ts
+++ client/src/services/rest.ts
@@ -5,6 +5,34 @@ export const REST_BASE_PATH = '/hjelpemidler/delbestilling'
 export const API_PATH = REST_BASE_PATH + '/api'
 export const ROLLER_PATH = REST_BASE_PATH + '/roller'
 
+export class ApiError extends Error {
+  statusCode: number | undefined
+
+  constructor(message: string, statusCode?: number) {
+    super(message)
+    this.statusCode = statusCode
+  }
+
+  isUnauthorized(): boolean {
+    return this.statusCode === 401
+  }
+
+  isForbidden(): boolean {
+    return this.statusCode === 403
+  }
+}
+
+const handleResponse = async (response: Response) => {
+  if (response.status >= 400 && response.status < 500) {
+    const error = await response.json().catch((err: unknown) => console.log('ApiError thrown in handleResponse()', err))
+    const message = error ? error.message : 'ApiError thrown in handleResponse()'
+    throw new ApiError(message, response.status)
+  }
+  if (!response.ok) {
+    throw new ApiError('Feil ved kontakt mot baksystem.', response.status)
+  }
+}
+
 const fetchPost: (url: string, otherParams?: any, timeout?: number) => Promise<Response> = (url, otherParams) => {
   return fetch(url, { method: 'POST', ...otherParams })
 }
@@ -17,16 +45,20 @@ const hjelpemiddelOppslag = async (artnr: string, serienr: string): Promise<Opps
     },
   })
 
+  await handleResponse(response)
+
   return await response.json()
 }
 
 const hentBestillingerForBruker = async (): Promise<InnsendtBestilling[]> => {
   const response = await fetch(API_PATH + '/delbestilling')
+  await handleResponse(response)
   return await response.json()
 }
 
 const hentBestillingerForKommune = async (): Promise<InnsendtBestilling[]> => {
   const response = await fetch(API_PATH + '/delbestilling/kommune')
+  await handleResponse(response)
   return await response.json()
 }
 
@@ -38,12 +70,15 @@ const sendInnBestilling = async (bestilling: InnsendtBestilling): Promise<string
     },
   })
 
+  await handleResponse(response)
+
   const id = await response.text()
   return id
 }
 
 const hentRolle = async (): Promise<DelbestillerResponse> => {
   const response = await fetch(`${ROLLER_PATH}/delbestiller`)
+  await handleResponse(response)
   return await response.json()
 }
 
