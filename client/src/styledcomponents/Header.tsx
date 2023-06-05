2f5610c275108d3ffe147da62ccb1469d937f0a7
Håkon <hakonhagen2@gmail.com>
2023-06-05 14:52:29 +0200

    header tweaks

--- client/src/styledcomponents/Header.tsx
+++ client/src/styledcomponents/Header.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
 const Header = styled.header`
-  padding: 4rem 0;
-  border-bottom: 3px solid #33aa5f;
+  padding: 2rem 0;
+  border-bottom: 3px solid #99c4dd;
+  text-align: center;
 `
 
 export default Header
