0cd9a95618b47b30153dfa4c5f59e1dcb6d7da7f
Håkon <hakonhagen2@gmail.com>
2023-06-16 10:44:53 +0200

    Vis Alert om testmiljø

--- client/src/containers/Layout.tsx
+++ client/src/containers/Layout.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Header from '../styledcomponents/Header'
 import Content from '../styledcomponents/Content'
-import { Heading } from '@navikt/ds-react'
+import { Alert, Heading } from '@navikt/ds-react'
 import { Outlet } from 'react-router-dom'
+import { Avstand } from '../components/Avstand'
 
 // Delte page-komponenter for hver side
 const Layout = () => {
+  const visTestMiljoBanner = window.appSettings.USE_MSW === true && window.location.hostname !== 'localhost'
   return (
     <>
       <Header>
         <Content>
+          {visTestMiljoBanner && (
+            <Avstand marginTop={4} marginBottom={8}>
+              <Alert variant="info">
+                Du ser nå på en testversjon av løsningen, hvor informasjonen kun er testdata og ikke gjenspeiler
+                virkeligheten.
+              </Alert>
+            </Avstand>
+          )}
           <Heading level="1" size="xlarge">
             Bestill del til hjelpemiddel
           </Heading>
