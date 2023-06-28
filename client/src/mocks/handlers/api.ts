a807e20c12468ed550c2107a20a46b5afbcfd765
Håkon <hakonhagen2@gmail.com>
2023-06-28 14:00:37 +0200

    La til http-status-codes

--- client/src/mocks/handlers/api.ts
+++ client/src/mocks/handlers/api.ts
@@ -11,6 +11,7 @@ import {
 import hjelpemiddelMock from '../../services/hjelpemiddel-mock.json'
 import hjelpemidlerMock from '../../services/hjelpemidler-mock.json'
 import { Delbestilling } from '../../types/Types'
+import { StatusCodes } from 'http-status-codes'
 
 import { API_PATH } from '../../services/rest'
 
@@ -21,13 +22,17 @@ const apiHandlers = [
     const { hmsnr } = req.body
 
     if (hmsnr === '333333') {
-      return res(ctx.delay(250), ctx.status(404), ctx.json({ hjelpemiddel: undefined, feil: OppslagFeil.INGET_UTLÅN }))
+      return res(
+        ctx.delay(250),
+        ctx.status(StatusCodes.NOT_FOUND),
+        ctx.json({ hjelpemiddel: undefined, feil: OppslagFeil.INGET_UTLÅN })
+      )
     }
 
     if (hmsnr === '000000') {
       return res(
         ctx.delay(250),
-        ctx.status(404),
+        ctx.status(StatusCodes.NOT_FOUND),
         ctx.json({ hjelpemiddel: undefined, feil: OppslagFeil.TILBYR_IKKE_HJELPEMIDDEL })
       )
     }
@@ -47,7 +52,7 @@ const apiHandlers = [
       !delbestilling.serienr ||
       !delbestilling.levering
     ) {
-      return res(ctx.status(400))
+      return res(ctx.status(StatusCodes.BAD_REQUEST))
     }
 
     const id = delbestilling.id
@@ -55,36 +60,46 @@ const apiHandlers = [
     tidligereBestillinger.push(delbestilling)
 
     if (delbestilling.serienr === '000000') {
-      return res(ctx.delay(450), ctx.status(404), ctx.json({ id, feil: DelbestillingFeil.BRUKER_IKKE_FUNNET }))
+      return res(
+        ctx.delay(450),
+        ctx.status(StatusCodes.NOT_FOUND),
+        ctx.json({ id, feil: DelbestillingFeil.BRUKER_IKKE_FUNNET })
+      )
     }
 
     if (delbestilling.hmsnr === '222222' && delbestilling.serienr === '111111') {
-      return res(ctx.delay(450), ctx.status(403), ctx.json({ id, feil: DelbestillingFeil.BESTILLE_TIL_SEG_SELV }))
+      return res(
+        ctx.delay(450),
+        ctx.status(StatusCodes.FORBIDDEN),
+        ctx.json({ id, feil: DelbestillingFeil.BESTILLE_TIL_SEG_SELV })
+      )
     }
 
     if (delbestilling.hmsnr === '222222' && delbestilling.serienr === '444444') {
-      return res(ctx.delay(450), ctx.status(403), ctx.json({ id, feil: DelbestillingFeil.ULIK_GEOGRAFISK_TILKNYTNING }))
+      return res(
+        ctx.delay(450),
+        ctx.status(StatusCodes.FORBIDDEN),
+        ctx.json({ id, feil: DelbestillingFeil.ULIK_GEOGRAFISK_TILKNYTNING })
+      )
     }
 
     if (delbestilling.hmsnr === '222222' && delbestilling.serienr === '555555') {
-      return res(ctx.delay(450), ctx.status(404), ctx.json({ id, feil: DelbestillingFeil.KAN_IKKE_BESTILLE }))
+      return res(
+        ctx.delay(450),
+        ctx.status(StatusCodes.NOT_FOUND),
+        ctx.json({ id, feil: DelbestillingFeil.KAN_IKKE_BESTILLE })
+      )
     }
 
     if (delbestilling.hmsnr === '222222' && delbestilling.serienr === '666666') {
       return res(
         ctx.delay(450),
-        ctx.status(403),
+        ctx.status(StatusCodes.FORBIDDEN),
         ctx.json({ id, feil: DelbestillingFeil.FOR_MANGE_BESTILLINGER_SISTE_24_TIMER })
       )
     }
 
-    // return res(ctx.delay(450), ctx.status(400))
-    // return res(ctx.delay(450), ctx.status(401))
-    // return res(ctx.delay(450), ctx.status(500))
-    // return res(ctx.delay(450), ctx.status(403), ctx.json({ id, feil: DelbestillingFeil.ULIK_GEOGRAFISK_TILKNYTNING }))
-    // return res(ctx.delay(450), ctx.status(404), ctx.json({ id, feil: DelbestillingFeil.INGET_UTLÅN }))
-
-    return res(ctx.delay(450), ctx.status(201), ctx.json({ id: delbestilling.id }))
+    return res(ctx.delay(450), ctx.status(StatusCodes.CREATED), ctx.json({ id: delbestilling.id }))
   }),
 
   rest.get<{}, {}, Delbestilling[]>(`${API_PATH}/delbestilling`, (req, res, ctx) => {
--- client/src/mocks/handlers/auth.ts
+++ client/src/mocks/handlers/auth.ts
@@ -1,8 +1,9 @@
 import { rest } from 'msw'
+import { StatusCodes } from 'http-status-codes'
 
 const authHandlers = [
   rest.get('/hjelpemidler/delbestilling/auth/status', (req, res, ctx) => {
-    return res(ctx.status(200))
+    return res(ctx.status(StatusCodes.OK))
   }),
 ]
 
