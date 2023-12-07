646c096f4b3ae4c7e24c2df442a19ed5081c55aa
Håkon <hakonhagen2@gmail.com>
2023-12-07 18:17:11 +0100

    vis link på kvittering

--- client/src/containers/Kvittering.tsx
+++ client/src/containers/Kvittering.tsx
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useLocation, useNavigate } from 'react-router-dom'
 
-import { Alert, Button, Heading, HStack } from '@navikt/ds-react'
+import { Alert, BodyShort, Button, Heading, HStack } from '@navikt/ds-react'
 
 import { Avstand } from '../components/Avstand'
 import BestillingsKort from '../components/BestillingsKort'
+import { useRolleContext } from '../context/rolle'
 import { GlobalStyle } from '../GlobalStyle'
 import Content from '../styledcomponents/Content'
 import { DelbestillingSak } from '../types/Types'
@@ -23,6 +24,7 @@ const Kvittering = () => {
   const navigate = useNavigate()
   const location = useLocation()
   const state = location.state as LocationState | null
+  const { delbestillerrolle } = useRolleContext()
 
   useEffect(() => {
     if (isProd()) {
@@ -60,12 +62,29 @@ const Kvittering = () => {
         )}
         {!delbestillingSak && <Alert variant="warning">{t('kvittering.fantIkkeKvittering')}</Alert>}
 
-        <Avstand marginTop={10} />
-        <HStack justify="center">
-          <Button variant="secondary" onClick={handleNyBestillingClick}>
-            {t('kvittering.startNyBestilling')}
-          </Button>
-        </HStack>
+        <Avstand marginTop={8} />
+
+        {delbestillerrolle.erTekniker && (
+          <>
+            <BodyShort>
+              Du kan se bestillinger du har sendt inn på{' '}
+              <a href="/hjelpemidler/delbestilling/bestillinger">Dine innsendte bestillinger.</a>
+            </BodyShort>
+            <Avstand marginBottom={14} />
+            <HStack justify="center">
+              <Button variant="secondary" onClick={handleNyBestillingClick}>
+                {t('kvittering.startNyBestilling')}
+              </Button>
+            </HStack>
+          </>
+        )}
+
+        {delbestillerrolle.erBrukerpassbruker && (
+          <BodyShort>
+            Du kan se bestillinger du har sendt inn på Ditt NAV på{' '}
+            <a href={window.appSettings.DINEHJELPEMIDLER_URL}>Hjelpemidler.</a>
+          </BodyShort>
+        )}
       </Content>
     </main>
   )
