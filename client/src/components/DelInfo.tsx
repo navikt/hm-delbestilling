a9342928c1c635b6646284f7db45ca8c96f76cdd
Håkon <hakonhagen2@gmail.com>
2025-04-10 19:32:58 +0200

    Update DelInfo.tsx

--- client/src/components/DelInfo.tsx
+++ client/src/components/DelInfo.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-import { ChevronLeftIcon, ChevronRightIcon, ImageIcon, MagnifyingGlassIcon } from '@navikt/aksel-icons'
+import { ChevronLeftIcon, ChevronRightIcon, ImageIcon, MagnifyingGlassFillIcon } from '@navikt/aksel-icons'
 import { BodyShort, Box, Button, Detail, Heading, HGrid, HStack, Modal } from '@navikt/ds-react'
 
 import { size } from '../styledcomponents/rules'
@@ -124,13 +124,13 @@ const Magnifyer = styled.div`
   position: absolute;
   top: 0;
   right: 0;
-  padding: 6px;
+  padding: 5px;
   background: rgba(0, 0, 0, 0.5);
+  border-radius: 0 0 0 50%;
 `
 
 const Karusell = ({ imgs, navn }: { imgs: string[]; navn: string }) => {
   const [valgtIndex, setValgtIndex] = useState<number>(-1)
-  console.log('valgtIndex:', valgtIndex)
 
   return (
     <>
@@ -143,7 +143,7 @@ const Karusell = ({ imgs, navn }: { imgs: string[]; navn: string }) => {
         <ImgWrap aria-hidden style={{ position: 'relative' }}>
           <img src={imgs[0]} alt={navn} />
           <Magnifyer>
-            <MagnifyingGlassIcon style={{ color: 'white' }} />
+            <MagnifyingGlassFillIcon style={{ color: 'white' }} />
           </Magnifyer>
         </ImgWrap>
       </ThumbnailButton>
