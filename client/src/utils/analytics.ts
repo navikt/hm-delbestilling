d6b48ad7ae6a5b70e4d7674e34a2ca89e84c908c
Håkon <hakonhagen2@gmail.com>
2025-07-29 13:28:09 +0200

    refactor

--- client/src/utils/analytics.ts
+++ client/src/utils/analytics.ts
@@ -45,10 +45,15 @@ export const initAmplitude = () => {
   }
 }
 
-export function logAmplitudeEvent(eventName: string, data?: any) {
+export const umamiWebsiteId = isProd()
+  ? '35abb2b7-3f97-42ce-931b-cf547d40d967' // Nav.no - prod
+  : '7ea31084-b626-4535-ab44-1b2d43001366' // hjelpemidler - dev
+
+function logEvent(eventName: string, data?: Record<string, unknown>) {
   if (!isConsentingToAnalytics()) {
     return
   }
+  // TODO: fjern sending til Amplitude når Umami funker som det skal
   setTimeout(() => {
     data = {
       app: 'hm-delbestilling',
@@ -64,92 +69,62 @@ export function logAmplitudeEvent(eventName: string, data?: any) {
       console.error(error)
     }
   })
-}
-
-export const umamiWebsiteId = isProd()
-  ? '35abb2b7-3f97-42ce-931b-cf547d40d967' // Nav.no - prod
-  : '7ea31084-b626-4535-ab44-1b2d43001366' // hjelpemidler - dev
-
-const logUmamiEvent = (event: string, data?: Record<string, unknown>) => {
-  if (!isConsentingToAnalytics()) {
-    return
-  }
 
   console.log('[debug] window.umami:', window.umami)
   if (window.umami && typeof window.umami.track === 'function') {
-    console.log('tracker data', data)
-    window.umami.track(event, data)
+    window.umami.track(eventName, data)
   }
 }
-
 export const logSpråkEndret = (språk: string) => {
-  logAmplitudeEvent(digihot_taxonomy.SPRÅK_ENDRET, { språk })
-  logUmamiEvent(digihot_taxonomy.SPRÅK_ENDRET, { språk })
+  logEvent(digihot_taxonomy.SPRÅK_ENDRET, { språk })
 }
 
 export const logOppslagGjort = (hmsnr: string) => {
-  logAmplitudeEvent(digihot_taxonomy.OPPSLAG_GJORT, { artnr: hmsnr })
-  logUmamiEvent(digihot_taxonomy.OPPSLAG_GJORT, { artnr: hmsnr })
+  logEvent(digihot_taxonomy.OPPSLAG_GJORT, { artnr: hmsnr })
 }
 
 export const logOppslagFeil = (oppslagFeil: string, hmsnr: string, statuskode?: number) => {
-  logAmplitudeEvent(digihot_taxonomy.OPPSLAG_FEIL, { oppslagFeil, artnr: hmsnr, statuskode })
-  logUmamiEvent(digihot_taxonomy.OPPSLAG_FEIL, { oppslagFeil, artnr: hmsnr, statuskode })
+  logEvent(digihot_taxonomy.OPPSLAG_FEIL, { oppslagFeil, artnr: hmsnr, statuskode })
 }
 
 export const logKategoriFiltreringGjort = (filter: string) => {
-  logAmplitudeEvent(digihot_taxonomy.KATEGORI_FILTRERING, { filter })
-  logUmamiEvent(digihot_taxonomy.KATEGORI_FILTRERING, { filter })
+  logEvent(digihot_taxonomy.KATEGORI_FILTRERING, { filter })
 }
 
 export const logKlikkVisKunFastLagervare = (checked: boolean) => {
-  logAmplitudeEvent(digihot_taxonomy.KLIKK_PÅ_VIS_KUN_FAST_LAGERVARE, {
-    checked,
-  })
-  logUmamiEvent(digihot_taxonomy.KLIKK_PÅ_VIS_KUN_FAST_LAGERVARE, {
+  logEvent(digihot_taxonomy.KLIKK_PÅ_VIS_KUN_FAST_LAGERVARE, {
     checked,
   })
 }
 
 export const logSkjemavalideringFeilet = (feil: string[] | undefined) => {
-  logAmplitudeEvent(nav_taxonomy.SKJEMAVALIDERING_FEILET, {
-    feil,
-  })
-  logUmamiEvent(nav_taxonomy.SKJEMAVALIDERING_FEILET, {
+  logEvent(nav_taxonomy.SKJEMAVALIDERING_FEILET, {
     feil,
   })
 }
 
 export const logInnsendingGjort = (id: string) => {
-  logAmplitudeEvent(nav_taxonomy.SKJEMA_FULLFØRT, { id })
-  logUmamiEvent(nav_taxonomy.SKJEMA_FULLFØRT, { id })
+  logEvent(nav_taxonomy.SKJEMA_FULLFØRT, { id })
 }
 
 export const logInnsendingFeil = (feil: string) => {
-  logAmplitudeEvent(digihot_taxonomy.INNSENDING_FEIL, { feil })
-  logUmamiEvent(digihot_taxonomy.INNSENDING_FEIL, { feil })
+  logEvent(digihot_taxonomy.INNSENDING_FEIL, { feil })
 }
 
 export const logBestillingSlettet = () => {
-  logAmplitudeEvent(digihot_taxonomy.SKJEMA_SLETTET)
-  logUmamiEvent(digihot_taxonomy.SKJEMA_SLETTET)
+  logEvent(digihot_taxonomy.SKJEMA_SLETTET)
 }
 
 export const logStartNyBestilling = () => {
-  logAmplitudeEvent(digihot_taxonomy.START_NY_BESTILING)
-  logUmamiEvent(digihot_taxonomy.START_NY_BESTILING)
+  logEvent(digihot_taxonomy.START_NY_BESTILING)
 }
 
 export const logPrintAvBestillingÅpnet = (pathname: string) => {
-  logAmplitudeEvent(digihot_taxonomy.PRINT_AV_BESTILLING_ÅPNET, {
-    pathname,
-  })
-  logUmamiEvent(digihot_taxonomy.PRINT_AV_BESTILLING_ÅPNET, {
+  logEvent(digihot_taxonomy.PRINT_AV_BESTILLING_ÅPNET, {
     pathname,
   })
 }
 
 export const logÅpningAvBildekarusell = () => {
-  logAmplitudeEvent(digihot_taxonomy.KLIKK_ÅPNING_AV_BILDEKARUSELL)
-  logUmamiEvent(digihot_taxonomy.KLIKK_ÅPNING_AV_BILDEKARUSELL)
+  logEvent(digihot_taxonomy.KLIKK_ÅPNING_AV_BILDEKARUSELL)
 }
