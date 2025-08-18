794a037921daadd3512fdca380913843a08fa039
Håkon <hakonhagen2@gmail.com>
2025-08-18 14:28:32 +0200

    Fjerner ubrukt kode, rydder litt

--- client/src/components/DelKategoriVelger.tsx
+++ client/src/components/DelKategoriVelger.tsx
@@ -1,53 +1,73 @@
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from 'react'
 
 import { Chips } from '@navikt/ds-react'
 
+import { Del } from '../types/Types'
 import { logKategoriFiltreringGjort } from '../utils/amplitude'
 
 interface Props {
-  delKategorier: string[] | undefined
+  delKategorier: string[]
   kategoriFilter?: string | undefined
   logKategoriValg?: boolean
   setKategoriFilter: Dispatch<SetStateAction<string | undefined>>
 }
 
 const DelKategoriVelger = ({ delKategorier, kategoriFilter, setKategoriFilter, logKategoriValg = true }: Props) => {
-  if (delKategorier) {
-    const visCheckmark = delKategorier.length <= 2
-    return (
-      <Chips>
+  const visCheckmark = delKategorier.length <= 2
+
+  return (
+    <Chips>
+      <Chips.Toggle
+        checkmark={visCheckmark}
+        key="alle-deler"
+        selected={kategoriFilter === undefined}
+        onClick={() => {
+          if (logKategoriValg) {
+            logKategoriFiltreringGjort('alle deler')
+          }
+          setKategoriFilter(undefined)
+        }}
+      >
+        Alle deler
+      </Chips.Toggle>
+      {delKategorier.map((kategori) => (
         <Chips.Toggle
           checkmark={visCheckmark}
-          key="alle-deler"
-          selected={kategoriFilter === undefined}
+          key={kategori}
+          selected={kategoriFilter === kategori}
           onClick={() => {
             if (logKategoriValg) {
-              logKategoriFiltreringGjort('alle deler')
+              logKategoriFiltreringGjort(kategori)
             }
-            setKategoriFilter(undefined)
+            setKategoriFilter(kategori)
           }}
         >
-          Alle deler
+          {kategori}
         </Chips.Toggle>
-        {delKategorier.map((kategori) => (
-          <Chips.Toggle
-            checkmark={visCheckmark}
-            key={kategori}
-            selected={kategoriFilter === kategori}
-            onClick={() => {
-              if (logKategoriValg) {
-                logKategoriFiltreringGjort(kategori)
-              }
-              setKategoriFilter(kategori)
-            }}
-          >
-            {kategori}
-          </Chips.Toggle>
-        ))}
-      </Chips>
-    )
-  }
-  return <></>
+      ))}
+    </Chips>
+  )
+}
+
+export const useDelKategorier = (deler: Del[] | undefined) => {
+  const [kategoriFilter, setKategoriFilter] = useState<string | undefined>()
+
+  const delKategorier = useMemo(() => {
+    if (!deler) {
+      return []
+    }
+
+    return deler.reduce((acc, del) => {
+      if (!acc.includes(del.kategori)) {
+        acc.push(del.kategori)
+      }
+      return acc
+    }, [] as string[])
+  }, [deler]).sort()
+
+  useEffect(() => setKategoriFilter(undefined), [deler])
+
+  return { kategoriFilter, setKategoriFilter, delKategorier }
 }
 
 export default DelKategoriVelger
--- client/src/components/LeggTilDel.tsx
+++ client/src/components/LeggTilDel.tsx
@@ -3,19 +3,16 @@ import { useTranslation } from 'react-i18next'
 
 import { Alert, BodyShort, Button, Detail, Heading, HStack, Search, Switch } from '@navikt/ds-react'
 
-import useDelKategorier from '../hooks/useDelKategorier'
 import { CustomBox } from '../styledcomponents/CustomBox'
 import FlexedStack from '../styledcomponents/FlexedStack'
 import { Del, Hjelpemiddel, Pilot } from '../types/Types'
 import { logKlikkVisKunFastLagervare } from '../utils/amplitude'
-import { triggerHotjarEvent } from '../utils/hotjar'
-import { isConsentingToSurveys } from '../utils/nav-cookie-consent'
 
 import { Avstand } from './Avstand'
 import { Beskrivelser } from './Beskrivelser'
 import { Bilde } from './Bilde'
 import DelInnhold from './DelInhold'
-import DelKategoriVelger from './DelKategoriVelger'
+import DelKategoriVelger, { useDelKategorier } from './DelKategoriVelger'
 
 interface Props {
   hjelpemiddel: Hjelpemiddel
@@ -25,17 +22,12 @@ interface Props {
 }
 const LeggTilDel = ({ hjelpemiddel, onLeggTil, knappeTekst = 'Legg til del', piloter }: Props) => {
   const { delKategorier, kategoriFilter, setKategoriFilter } = useDelKategorier(hjelpemiddel.deler)
+
   const { t } = useTranslation()
   const [visKunDigitaleDeler, setVisKunDigitaleDeler] = useState(false)
   const [søk, setSøk] = useState('')
 
   const erPilotForBestilleIkkeFasteLagervarer = piloter.includes(Pilot.BESTILLE_IKKE_FASTE_LAGERVARER)
-
-  const handleClickManglerDel = () => {
-    if (isConsentingToSurveys()) {
-      triggerHotjarEvent('digihot_delbestilling_mangler_del_feedback')
-    }
-  }
 
   if (!hjelpemiddel.deler || hjelpemiddel.deler.length === 0) {
     return <Alert variant="info">{t('leggTilDel.ingenDeler')}</Alert>
@@ -46,6 +38,7 @@ const LeggTilDel = ({ hjelpemiddel, onLeggTil, knappeTekst = 'Legg til del', pil
       <Heading size="medium" level="3" spacing>
         Deler til {hjelpemiddel.navn}
       </Heading>
+
       <Avstand marginBottom={2}>
         <DelKategoriVelger
           setKategoriFilter={setKategoriFilter}
@@ -146,16 +139,6 @@ const LeggTilDel = ({ hjelpemiddel, onLeggTil, knappeTekst = 'Legg til del', pil
             </Avstand>
           )
         })}
-      {/* {isConsentingToSurveys() && (
-        <Box borderColor="border-default" padding="8" borderWidth="2" style={{ borderStyle: 'dashed' }}>
-          <Avstand centered>
-            <BodyShort spacing>
-              <strong>Finner du ikke delen du er ute etter?</strong>
-            </BodyShort>
-            <Button onClick={handleClickManglerDel}>Fortell oss om det</Button>
-          </Avstand>
-        </Box>
-      )} */}
     </>
   )
 }
