2fe584fa454241a1c5029bdf97efefebc2a30419
Håkon <hakonhagen2@gmail.com>
2025-08-21 10:10:58 +0200

    vær sikker på at cookies er godtatt før umami-event sendes

--- client/src/utils/analytics/umami.ts
+++ client/src/utils/analytics/umami.ts
@@ -1,3 +1,4 @@
+import { isConsentingToAnalytics } from '../nav-cookie-consent'
 import { isProd } from '../utils'
 
 import { DIGIHOT_TAXONOMY, NAV_TAXONOMY, SKJEMANAVN } from './analytics'
@@ -7,6 +8,10 @@ export const UMAMI_WEBSITE_ID = isProd()
   : '7ea31084-b626-4535-ab44-1b2d43001366' // hjelpemidler - dev
 
 export const sendUmamiEvent = (eventName: NAV_TAXONOMY | DIGIHOT_TAXONOMY, data?: Record<string, unknown>) => {
+  if (!isConsentingToAnalytics()) {
+    return
+  }
+
   if (window.umami && typeof window.umami.track === 'function') {
     window.umami.track(eventName, {
       appNavn: SKJEMANAVN,
