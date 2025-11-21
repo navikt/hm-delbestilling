225c3a4afef8c843407ac1198e7eb0fe8a591b69
murvold <mads.severin.murvold@nav.no>
2025-11-21 15:19:33 +0100

    Fjerner utdatert endepunkt i whitelist

--- server/main.go
+++ server/main.go
@@ -46,7 +46,7 @@ func main() {
 		IDP:     idp,
 		EnvKeys: []string{},
 		ProtectedWhitelist: &texas.WhitelistConfig{
-			WhitelistPaths: []string{"/", "/bestillinger", "/utsjekk", "/api/oppslag", "/api/tilgjengelige-hjelpemidler", "/api/deler-til-hmsnrs", "/api/hjelpemiddel-titler"},
+			WhitelistPaths: []string{"/", "/bestillinger", "/utsjekk", "/api/oppslag", "/api/tilgjengelige-hjelpemidler", "/api/deler-til-hmsnrs"},
 		},
 	}
 	hotbff.Start(opts)
