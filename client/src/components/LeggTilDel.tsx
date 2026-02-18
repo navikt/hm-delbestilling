cfebcb4269c90c7e578b2880d12d119bd4ecd963
Zandra7 <sandra.eng@live.no>
2026-02-18 13:09:38 +0100

    Endrer på avstander

--- client/src/components/LeggTilDel.tsx
+++ client/src/components/LeggTilDel.tsx
@@ -96,14 +96,16 @@ const LeggTilDel = ({ hjelpemiddel, onLeggTil, knappeTekst = 'Legg til del' }: P
                         {del.navn}
                       </Heading>
 
-                      <HStack gap="space-20">
-                        <BodyShort textColor="subtle">HMS-nr. {del.hmsnr}</BodyShort>
-                        {del.levArtNr && <BodyShort textColor="subtle">Lev.art.nr. {del.levArtNr}</BodyShort>}
-                        <VStack>
-                          <BodyShort>{getDelType()}</BodyShort>
+                      <VStack gap="space-12">
+                        <HStack gap="space-20">
+                          <BodyShort textColor="subtle">HMS-nr. {del.hmsnr}</BodyShort>
+                          {del.levArtNr && <BodyShort textColor="subtle">Lev.art.nr. {del.levArtNr}</BodyShort>}
+                        </HStack>
+                        <VStack gap="space-6">
+                          <BodyShort textColor="subtle">{getDelType()}</BodyShort>
                           <BodyShort>{erFastLagervare ? 'Lagervare' : 'Bestillingsvare'}</BodyShort>
                         </VStack>
-                      </HStack>
+                      </VStack>
 
                       {harNyligBlittBestiltBatteri && hjelpemiddel.antallDagerSidenSistBatteribestilling !== null ? (
                         <Avstand marginTop={5}>
