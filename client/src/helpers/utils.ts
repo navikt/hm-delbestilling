2af8b982bfb93e6ffa34cf14b2b7ddd429f8aba2
Ole Steinar Lillestøl Skrede <3756410+olesls@users.noreply.github.com>
2026-06-26 13:14:30 +0200

    funksjon for sjekk på serienummerstyrt

--- client/src/helpers/utils.ts
+++ client/src/helpers/utils.ts
@@ -4,3 +4,25 @@ export const formaterNorskDato = (dato: string) =>
     month: 'long',
     year: 'numeric',
   })
+
+export const erSerienummerstyrt = (isokode: string): boolean => {
+  const serienummerstyrteIso4koder = [
+    '1222', // Manuelle rullestoler
+    '1223', // Elektriske rullestoler
+    '1236', // Personløftere
+    '1830', // Heiser og løfteplattformer
+  ]
+
+  const serienummerstyrteIso6koder = [
+    "181204", // "Senger og løse sengebunner/støtteplater for madrass uten reguleringsmulighet",
+    "181207", // "Senger og løse sengebunner/støtteplater for madrass med manuell regulering",
+    "181210", // "Senger og løse sengebunner/støtteplater for madrass med elektrisk regulering",
+    "220318", // "Bildeforstørrende videosystemer (lese-TV)",
+  ]
+
+  const iso4kode = isokode.substring(0, 4)
+  const iso6kode = isokode.substring(0, 6)
+
+  return serienummerstyrteIso4koder.includes(iso4kode) || serienummerstyrteIso6koder.includes(iso6kode)
+
+}
