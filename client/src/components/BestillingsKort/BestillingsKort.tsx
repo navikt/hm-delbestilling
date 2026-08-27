680a72dc8fc2efdcba6b51dbcaee81dea5af9326
Christian Bjørnsrud <christian.kristensen.bjornsrud@nav.no>
2026-08-27 15:08:30 +0200

    Legg til bestillers epost på kvittering om dette er lagt inn.

--- client/src/components/BestillingsKort/BestillingsKort.tsx
+++ client/src/components/BestillingsKort/BestillingsKort.tsx
@@ -50,6 +50,11 @@ const BestillingsKort = ({ sak }: Props) => {
           <Detail>Art.nr. {sak.delbestilling.hmsnr}</Detail>
           {sak.delbestilling.serienr && <Detail>Serienr. {sak.delbestilling.serienr}</Detail>}
           {sak.delbestilling.brukernr && <Detail>Brukernr. {sak.delbestilling.brukernr}</Detail>}
+          {sak.delbestilling.epostTekniker && (
+            <Detail>
+              {t('bestillinger.bestillersEpost')}: {sak.delbestilling.epostTekniker}
+            </Detail>
+          )}
         </HStack>
         <Avstand marginBottom={16} />
         {sak.delbestilling.deler.map((dellinje, index) => (
